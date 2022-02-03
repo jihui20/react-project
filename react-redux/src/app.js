f5f7ad4e54638f2e6b840d1b0e21276bdf28e8f3
이지희 <dlwlgml20@gmail.com>
2022-02-04 00:45:53 +0900

    피쳐명 -> 220204
    함수 컴포넌트

--- react-redux/src/app.js
+++ react-redux/src/app.js
@@ -1,18 +1,29 @@
 /* @jsx createElement */
 
-import { createDOM, createElement, render } from './react';
+// JSX의 Transpiler가 어떻게 동작 되는지 궁금하면? babel 페이지 -> Try it out
+// React.createElement가 default인데 지금은 React 객체가 없어서 에러
+// babel 안에 포함되어 있는 React의 JSX Transpiler가 옵션을 입력 받음
+// -> 소스코그 안 주석에 '@jsx'라고 하는 구문을 포함 시켜놓으면 그걸 입력으로 받아서 변환 결과를 변경시킬 수 있음
+
+import { createElement, render } from './react';
+
+function Title(props) {
+  return <h1>{props.children}</h1>;
+}
+
+function Item(props) {
+  return <li style={`color:${props.color}`}>{props.children}</li>;
+}
 
 const vdom = (
   <p>
-    <h1>React 만들기</h1>
+    <Title>React 정말 잘 만들기</Title>
     <ul>
-      <li style="color:red;">첫번째 아이템</li>
-      <li style="color:blue;">두번째 아이템</li>
-      <li style="color:green;">세번째 아이템</li>
+      <Item color="pink">첫 번째 아이템</Item>
+      <Item color="gray">드 번째 아이템</Item>
+      <Item color="purple">세 번째 아이템</Item>
     </ul>
   </p>
 );
-
-console.log(vdom);
 
 render(vdom, document.querySelector('#root'));
--- react-redux/src/react.js
+++ react-redux/src/react.js
@@ -15,9 +15,27 @@ export function createDOM(node) {
 }
 
 export function createElement(tag, props, ...children) {
-  props = props || {};
+  props = props || {}; // props가 null로 넘어올 경우에는 객체 반환
 
-  return { tag, props, children };
+  // 넘어온 tag가 함수라면? 함수 반환 아니면 기존대로 반환
+  // 함수는 jsx를 리턴, 호출되면 똑같이 '{ tag, props, children }' 객체가 반환
+
+  if (typeof tag === 'function') {
+    // props에 children이 넘어오면 props에다가 children이라는 키를 만들어서 넣어주기
+    //
+    if (children.length > 0) {
+      return tag({
+        ...props, // 기존 props는 그대로 전달
+        children: children.length === 1 ? children[0] : children,
+        // children의 length가 1이면 children의 첫번째에 있는 요소를 그대로 가져옴
+        // 1이 아니면  배열로 넘겨주기
+      });
+    } else {
+      return tag(props);
+    }
+  } else {
+    return { tag, props, children };
+  }
 }
 
 export function render(vdom, container) {
