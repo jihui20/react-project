6cca39764be2bc99a0ede685bf8013820d33e61b
이지희 <dlwlgml20@gmail.com>
2022-02-04 21:16:33 +0900

    - 불필요 파일 삭제 - 자바스크립트 스터디

---
+++ javascript/node.js
@@ -0,0 +1,8 @@
+// node는 브라우저에서 javascript를 실행한게 아니라
+// 컴퓨터 시스템, 운영체제에서 실행한것
+
+let colors = require('colors');
+
+console.log('hello jh'.underline.green);
+
+// 터미널에 node node.js 입력 -> node.js를 실행시켜라
