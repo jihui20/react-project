dc3ff4f1b54d13bbeea092a44954f90632528064
leejihui <dlwlgml20@yanadoo.co.kr>
2022-02-11 22:41:07 +0900

    퍼스트 캠퍼스 스터디 노트

---
+++ javascript/app.js
@@ -0,0 +1,21 @@
+// 값(value)
+const Color = {
+  Red: 1,
+  Blue: 2,
+  Green: 3,
+};
+
+Color.Red;
+
+// 타입
+function addAge(age) {
+  return age + 1;
+}
+
+let age = addAge(30);
+
+let a = 10;
+a = 50;
+
+let b = a;
+b = 20;
